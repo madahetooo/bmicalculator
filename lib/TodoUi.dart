0b404f81e5063f3beaa21c372f2f878bfdc336b9
madahetooo <ieslammedhat@gmail.com>
2022-08-09 16:12:40 +0200

    Adding Database Capability

--- lib/TodoUi.dart
+++ lib/TodoUi.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dbhelper.dart';
+
 class TodoUi extends StatefulWidget {
   const TodoUi({Key? key}) : super(key: key);
 
@@ -8,88 +10,199 @@ class TodoUi extends StatefulWidget {
 }
 
 class _TodoUiState extends State<TodoUi> {
+  final dbhelper = Databasehelper.instance;
+  final texteditingcontroller = TextEditingController();
+  bool validated = true;
+  String errtext = "";
+  String todoedited = "";
+  var myitems = [];
+  List<Widget> children = <Widget>[];
+
+  void addtodo() async {
+    Map<String, dynamic> row = {
+      Databasehelper.columnName: todoedited,
+    };
+    final id = await dbhelper.insert(row);
+    print(id);
+    Navigator.pop(context);
+    todoedited = "";
+    setState(() {
+      validated = true;
+      errtext = "";
+    });
+  }
+
+  Future<bool> query() async {
+    myitems = [];
+    children = [];
+    var allrows = await dbhelper.queryall();
+    allrows?.forEach((row) {
+      myitems.add(row.toString());
+      children.add(Card(
+        elevation: 5.0,
+        margin: EdgeInsets.symmetric(
+          horizontal: 10.0,
+          vertical: 5.0,
+        ),
+        child: Container(
+          padding: EdgeInsets.all(5.0),
+          child: ListTile(
+            title: Text(
+              row['todo'],
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            onLongPress: () {
+              dbhelper.deletedata(row['id']);
+              setState(() {});
+            },
+          ),
+        ),
+      ));
+    });
+    return Future.value(true);
+  }
+
+  void showalertdialog() {
+    texteditingcontroller.text = "";
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              title: Text(
+                "Add Task",
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextField(
+                    controller: texteditingcontroller,
+                    autofocus: true,
+                    onChanged: (_val) {
+                      todoedited = _val;
+                    },
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: "Raleway",
+                    ),
+                    decoration: InputDecoration(
+                      errorText: validated ? null : errtext,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 10.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton(
+                          onPressed: () {
+                            if (texteditingcontroller.text.isEmpty) {
+                              setState(() {
+                                errtext = "Can't Be Empty";
+                                validated = false;
+                              });
+                            } else if (texteditingcontroller.text.length >
+                                512) {
+                              setState(() {
+                                errtext = "Too may Chanracters";
+                                validated = false;
+                              });
+                            } else {
+                              addtodo();
+                            }
+                          },
+                          color: Colors.purple,
+                          child: const Text("ADD",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          });
+        });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text(
-          "TodoList App",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.black,
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: showAlertDialog,
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        backgroundColor: Colors.purple,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            myCard("Record a Video"),
-            myCard("Go To Gym"),
-            myCard("Go To Sleep"),
-            myCard("Take the money from Mamdouh"),
-            myCard("Visit Degla Apartment"),
-            myCard("Pay Fees to Yehia"),
-            myCard("Prepare for Flutter Session"),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget myCard(String task) {
-    return Card(
-      elevation: 5.0,
-      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-      child: Container(
-        padding: EdgeInsets.all(5.0),
-        child: ListTile(
-          title: Text("$task",style: TextStyle(
-            fontSize: 18.0,
-          ),),
-          onLongPress: (){
-            print("Should get deleted");
-          },
-        ),
-      ),
-    );
-  }
-
-  void showAlertDialog(){
-    showDialog(context: context, builder: (context) => AlertDialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      title: const Text(
-        "Add Task"
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const TextField(
-            autofocus: true,
-            style: TextStyle(
-              fontSize: 10.0,
+    return FutureBuilder(
+      builder: (context, snap) {
+        if (snap.hasData == null) {
+          return Center(
+            child: Text(
+              "No Data",
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // ignore: deprecated_member_use
-              RaisedButton(onPressed: (){},
-                color: Colors.purple,
-              child: const Text("Add"),)
-            ],
-          )
-        ],
-      ),
-    ));
+          );
+        } else {
+          if (myitems.length == 0) {
+            return Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: showalertdialog,
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.purple,
+              ),
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                centerTitle: true,
+                title: Text(
+                  "My Tasks",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              backgroundColor: Colors.black,
+              body: Center(
+                child: Text(
+                  "No Task Avaliable",
+                  style: TextStyle( fontSize: 20.0),
+                ),
+              ),
+            );
+          } else {
+            return Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: showalertdialog,
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                backgroundColor: Colors.purple,
+              ),
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                centerTitle: true,
+                title: const Text(
+                  "My Tasks",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              backgroundColor: Colors.black,
+              body: SingleChildScrollView(
+                child: Column(
+                  children: children,
+                ),
+              ),
+            );
+          }
+        }
+      },
+      future: query(),
+    );
   }
 }
